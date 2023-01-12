6f8480f3d0706394627f72534125b9c6749a4a2c
Lucas <lucas151@hotmail.com>
2023-01-12 01:02:35 -0300

    Configuração para desenvolvimento remoto

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@ export const PORT     = process.env.PORT     || 3000;
 export const DBHOST   = process.env.DBHOST   || 'localhost';
 export const DBNAME   = process.env.DBNAME   || 'my_despesas';
 export const DBUSER   = process.env.DBUSER   || 'root';
-export const DBPASS   = process.env.DBPASS   || '';
+export const DBPASS   = process.env.DBPASS   || 'Luc4ss4nt@s';
